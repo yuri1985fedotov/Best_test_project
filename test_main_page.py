e5691ed86154018521be329f50712ba5b8ad0b55
yuri1985fedotov <neoplan18785@mail.ru>
2025-09-30 11:37:05 +0300

    +проверка страницы логина

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -13,3 +14,9 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+def test_guest_can_go_be_login_page(browser):
+    link = "https://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
