45a3a0eb36b96d7fca11f10b10c05caf67ab9d75
yuri1985fedotov <neoplan18785@mail.ru>
2025-10-08 14:43:43 +0300

    Добавил отрицательные методы проверки

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,7 @@
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import math
@@ -28,4 +32,21 @@ class BasePage():
             print(f"Your code: {alert_text}")
             alert.accept()
         except NoAlertPresentException:
-            print("No second alert presented")    
+            print("No second alert presented")
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False     
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+            until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True   
