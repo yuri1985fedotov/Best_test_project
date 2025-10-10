75608828580a0481ceec9674d724ab91b4e1ee8f
yuri1985fedotov <neoplan18785@mail.ru>
2025-10-10 15:36:58 +0300

    Наследование и отрицательные проверки, добавил тест

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 import pytest
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 import time
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -44,3 +45,13 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 	page.go_to_login_page()
 	login_page = LoginPage(browser, browser.current_url)
 	login_page.should_be_login_page()        
+
+@pytest.mark.new     
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.basket_should_be_empty()
+    basket_page.should_be_message_about_empty_basket()     
