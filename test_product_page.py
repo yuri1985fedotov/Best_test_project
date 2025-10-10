25b4711bf999b5b7282651d5bb364455201d982b
yuri1985fedotov <neoplan18785@mail.ru>
2025-10-10 13:30:06 +0300

    Пример наследования, добавил новые тесты

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 import pytest
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 import time
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -15,11 +16,31 @@ import time
 
 def test_guest_can_add_product_to_basket(browser, link):
     #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open() # открываем страницу
+    page.should_not_be_success_message()  #ожидаем, что там нет сообщения об успешном добавлении в корзину
+    page.add_product_to_basket() # добавляем в корзину
+    page.solve_quiz_and_get_code() #Посчитать результат математического выражения и ввести ответ
+    time.sleep(2)
+    page.check_product_name() #Название товара в сообщении должно совпадать с тем товаром, который вы действительно добавили
+    page.check_product_price() #Цена товара в сообщении должно совпадать с тем товаром, который вы действительно добавили
+    page.should_disappear() #элемент присутствует на странице и должен исчезнуть
+
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
-    page.add_product_to_basket()
-    page.solve_quiz_and_get_code()
-    time.sleep(3)
-    page.check_product_name()
-    page.check_product_price()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+	link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+	page = ProductPage(browser, link)
+	page.open()
+	page.should_be_login_link()
+	page.go_to_login_page()
+	login_page = LoginPage(browser, browser.current_url)
+	login_page.should_be_login_page()        
