0efb03895d957bd768234267c1f968d1930af8b7
yuri1985fedotov <neoplan18785@mail.ru>
2025-10-10 15:35:51 +0300

    Наследование и отрицательные проверки, добавил тест

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,9 @@
+import pytest
 from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 from .pages.login_page import LoginPage
-
+from .pages.basket_page import BasketPage
+from .pages.base_page import BasePage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -22,4 +24,14 @@ def test_guest_can_go_be_login_page(browser):
     link = "https://selenium1py.pythonanywhere.com/ru/accounts/login/"
     page = LoginPage(browser, link)
     page.open()
-    page.should_be_login_page()    
+    page.should_be_login_page() 
+
+@pytest.mark.new
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = BasePage(browser, link)
+    page.open()
+    page.go_to_basket()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.basket_should_be_empty()
+    basket_page.should_be_message_about_empty_basket()
