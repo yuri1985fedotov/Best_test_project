3ef642d1cad1e5bf01081b3aeae78f230f25a130
yuri1985fedotov <neoplan18785@mail.ru>
2025-10-13 16:53:32 +0300

    Группировка тестов и setup, регистрация новых пользователей

--- pages/base_page.py
+++ pages/base_page.py
@@ -62,3 +62,7 @@ class BasePage():
     def go_to_basket(self):
         link = self.browser.find_element(*BasePageLocators.BASKET_LINK)
         link.click()
+
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "Значок пользователя не найден," \
+                                                                     "вероятно, неавторизованный пользователь"    
