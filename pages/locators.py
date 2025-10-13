d00aedfb0dd249077a4e151da96ae231f87fecbb
yuri1985fedotov <neoplan18785@mail.ru>
2025-10-13 16:54:26 +0300

    Группировка тестов и setup, регистрация новых пользователей

--- pages/locators.py
+++ pages/locators.py
@@ -6,6 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():  
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By. CSS_SELECTOR, "#register_form")
+    EMAIL_FIELD = (By.CSS_SELECTOR,"#register_form input[type=email]")
+    PASSWORD_FIELD = (By.CSS_SELECTOR,"#register_form input[name=registration-password1]")
+    PASSWORD_CONFIRM = (By.CSS_SELECTOR,"#register_form input[name=registration-password2]")
+    REG_BUTTON = (By.CSS_SELECTOR,"button[name=registration_submit]")
 
 class ProductPageLocators():
     BASKET_BUTTON = (By.CSS_SELECTOR, "#add_to_basket_form")
@@ -16,4 +20,10 @@ class ProductPageLocators():
 
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")    
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".btn-group a.btn.btn-default")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user") 
+
+class BasketPageLocators():
+    BASKET_ITEMS = (By.CSS_SELECTOR,".basket-items")
+    EMPTY_MESSAGE =(By.CSS_SELECTOR,"#content_inner>p")    
