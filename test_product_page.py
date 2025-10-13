84245ff809855395ec730446e6068a4e0c627612
yuri1985fedotov <neoplan18785@mail.ru>
2025-10-13 16:50:01 +0300

    Группировка тестов и setup, регистрация новых пользователей

--- test_product_page.py
+++ test_product_page.py
@@ -44,9 +44,9 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
 	page.should_be_login_link()
 	page.go_to_login_page()
 	login_page = LoginPage(browser, browser.current_url)
-	login_page.should_be_login_page()        
+	login_page.should_be_login_page()
 
-@pytest.mark.new     
+     
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = ProductPage(browser, link)
@@ -54,4 +54,32 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     page.go_to_basket()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.basket_should_be_empty()
-    basket_page.should_be_message_about_empty_basket()     
+    basket_page.should_be_message_about_empty_basket()             
+
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        login_page=LoginPage(browser, link)
+        login_page.open()
+        login_page.go_to_login_page()
+        email = str(time.time()) + "@fakemail.org"
+        password = "128mb256kb512gb"
+        login_page.register_new_user(email, password)
+        login_page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+        page = ProductPage(browser, link)
+        page.open()
+        page.add_product_to_basket()
+        page.solve_quiz_and_get_code()
+        page.check_product_name()
+        page.check_product_price()
